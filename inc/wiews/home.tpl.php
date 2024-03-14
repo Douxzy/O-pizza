43d78afcb057814624828a05dc806997d8733fd9
Douxzy <romain.descha2006@gmail.com>
2024-03-14 14:07:28 +0100

    Structure HTML en dur Fini

---
+++ inc/wiews/home.tpl.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>O'pizza</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+  <div class='container'>
+    <h1>Bienvenue chez O'Pizza</h1>
+
+    <p>Voici la liste des meilleures pizzas, commandez celle que vous voulez</p>
+
+    <div class="card-deck">
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="inc/image/pîzza.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Pizza napolitaine</h5>
+        <p class="card-text">champignon, roquefort, roquette</p>
+        <p class="card-text">8 €</p>
+      </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="inc/image/pîzza.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Pizza sicilienne</h5>
+        <p class="card-text">buratta, truffe</p>
+        <p class="card-text">15 €</p>
+      </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="inc/image/pîzza.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Pizza New York style</h5>
+        <p class="card-text">cheddar</p>
+        <p class="card-text">10 €</p>
+      </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
